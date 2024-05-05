313b32d307059a286d617056c4111bfaed49b0c4
Anand Patel <arms11@gmail.com>
2024-05-04 21:46:54 -0400

    Adds tests

--- check_prime.py
+++ check_prime.py
@@ -1,6 +1,6 @@
 def is_prime(num: int):
   if num > 1:
-    for i in range(2, (num//2)+1):
+    for i in range(2, num+1):
       if (num % i) == 0:
         return True
-  return False  
+  return False
---
+++ test_check_prime.py
@@ -0,0 +1,5 @@
+from check_prime import is_prime
+
+def test_answer():
+    assert is_prime(1) == False
+    assert is_prime(2) == True
